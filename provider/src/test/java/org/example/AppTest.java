d409ea422089fc409b892a5b8411d5bb83061b8e
chenzq <chenzq2@allinpay.com>
2024-04-23 17:02:21 +0800

    补充

---
+++ provider/src/test/java/org/example/AppTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+import java.util.Properties;
+import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Unit test for simple App.
+ */
+
+@SpringBootTest
+public class AppTest {
+
+
+}
